414bdae67c5d500e27e4e782d7b51e28be8e1300
Yura <YuraLysenko@users.noreply.github.com>
2019-05-23 17:59:30 +0300

    add scroll jquery

--- dronezone/js/all.js
+++ dronezone/js/all.js
@@ -3305,3 +3305,15 @@ function initMap() {
 
 }
 
+///scroll
+$(document).ready(function(){
+    $("a[href*=#]").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
+
